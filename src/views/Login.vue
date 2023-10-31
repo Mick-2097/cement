5c4dd766d4f0dcdfac39b517f9cc775a7d5a8a16
Mick-2097 <mix2444@gmail.com>
2023-10-31 20:00:19 +1100

    x

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,8 +2,8 @@
 import Vbutton from '../components/Vbutton.vue'
 import Keycloak from 'keycloak-js'
 const keycloak = new Keycloak({
-    // url: 'https://auth.dev.nlplay.ru/auth/',
-    url: 'http://localhost:5173/login',
+    url: 'https://auth.dev.nlplay.ru/auth/',
+    // url: 'http://localhost:5173/login',
     realm: 'proptech',
     clientId: 'smc'
 })
